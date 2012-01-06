127150a5f6e41b33315cb4e23763dd4f16a34296
Xurxo Méndez Pérez <sonxurxo@gmail.com>
2012-01-06 11:22:37 +0100

    Initial Commit

---
+++ DicionarioGalego/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DicionarioGalego
+//
+//  Created by Xurxo Méndez Pérez on 06/01/12.
+//  Copyright (c) 2012 ninguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DicionarioGalego/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DicionarioGalego
+//
+//  Created by Xurxo Méndez Pérez on 06/01/12.
+//  Copyright (c) 2012 ninguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
